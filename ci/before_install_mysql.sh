b05e0a3d29ba106ef624e309845df189418fe7cd
avraham <tovmeod@gmail.com>
2016-01-18 17:18:37 +0200

    trying to fix mysql on travis

--- ci/before_install_mysql.sh
+++ ci/before_install_mysql.sh
@@ -1,1 +1,4 @@
 pip install -q mysql-python
+echo -e "[server]\nmax_allowed_packet=64M" | sudo tee -a /etc/mysql/conf.d/fix.cnf
+sudo service mysql reload
+mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
