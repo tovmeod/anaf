ac31202d98f097c19411ff949db6ef8f15eee950
avraham <tovmeod@gmail.com>
2016-01-18 18:30:05 +0200

    trying tweking wait_timeout

--- ci/before_install_mysql.sh
+++ ci/before_install_mysql.sh
@@ -1,4 +1,8 @@
 pip install -q mysql-python
+mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
+mysql -e "SHOW VARIABLES LIKE 'wait_timeout';"
 echo -e "[server]\nmax_allowed_packet=128M" | sudo tee -a /etc/mysql/conf.d/fix.cnf
+echo -e "wait_timeout = 600" | sudo tee -a /etc/mysql/conf.d/fix.cnf
 sudo service mysql restart
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
+mysql -e "SHOW VARIABLES LIKE 'wait_timeout';"
