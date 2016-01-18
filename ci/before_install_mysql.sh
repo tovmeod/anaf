31a93272be17276e07bd60d00937208a48639d7a
avraham <tovmeod@gmail.com>
2016-01-18 17:47:35 +0200

    trying without sudo

--- ci/before_install_mysql.sh
+++ ci/before_install_mysql.sh
@@ -1,4 +1,4 @@
 pip install -q mysql-python
-echo -e "[server]\nmax_allowed_packet=64M" | sudo tee -a /etc/mysql/conf.d/fix.cnf
-sudo service mysql restart
+echo -e "[server]\nmax_allowed_packet=64M" | tee -a /etc/mysql/conf.d/fix.cnf
+service mysql restart
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
