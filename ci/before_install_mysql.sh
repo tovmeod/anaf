636e7981f8989efb993b2c6c50b7cfd4f4b68e4c
avraham <tovmeod@gmail.com>
2016-01-18 19:17:24 +0200

    increasing mysql wait_timeout

--- ci/before_install_mysql.sh
+++ ci/before_install_mysql.sh
@@ -2,6 +2,6 @@ pip install -q mysql-python
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
 mysql -e "SHOW VARIABLES LIKE 'wait_timeout';"
 mysql -e "SET GLOBAL max_allowed_packet=134217728;"
-mysql -e "SET GLOBAL wait_timeout=600;"
+mysql -e "SET GLOBAL wait_timeout=6000;"
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
 mysql -e "SHOW VARIABLES LIKE 'wait_timeout';"
