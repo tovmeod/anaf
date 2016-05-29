22fa61956404f8dd2ed43559812a9d615e40faac
avraham <tovmeod@gmail.com>
2016-05-29 21:18:03 +0300

    fixing some docstring issues (PEP257)

--- anaf/projects/widgets.py
+++ anaf/projects/widgets.py
@@ -1,12 +1,10 @@
-"""
-Projects module widgets
-"""
+"""Projects module widgets."""
 
 WIDGETS = {'widget_tasks_assigned_to_me': {'title': 'Tasks Assigned To Me',
                                            'size': "95%"}}
 
 
 def get_widgets(request):
-    "Returns a set of all available widgets"
+    """Return a set of all available widgets."""
 
     return WIDGETS
