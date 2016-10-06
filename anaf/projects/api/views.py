ddf1a63f745a77739d13cf9dbfacdc607e5fedd2
avraham <tovmeod@gmail.com>
2016-10-06 07:59:01 +0300

    initial implementation of project view using DRF class supporting html and json

--- anaf/projects/api/views.py
+++ anaf/projects/api/views.py
@@ -1,8 +1,14 @@
+from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.response import Response
 
+from anaf.core.models import Object
 from anaf.projects.api.serializers import TaskTimeSlotSerializer
+from anaf.projects.forms import FilterForm
 from anaf.projects.models import Project, TaskStatus, Milestone, Task, TaskTimeSlot
 from anaf.projects.api.serializers import ProjectSerializer, TaskStatusSerializer, MilestoneSerializer, TaskSerializer
+from anaf.projects.views import _get_default_context, _get_filter_query
+from anaf.core.ajax.converter import preprocess_context
 
 
 class ProjectView(viewsets.ModelViewSet):
@@ -11,6 +17,30 @@ class ProjectView(viewsets.ModelViewSet):
     """
     queryset = Project.objects.all().order_by('-date_created')
     serializer_class = ProjectSerializer
+    template_name = 'projects/index.html'
+    accepted_formats = ('html', 'ajax')
+
+    def list(self, request, *args, **kwargs):
+        if request.accepted_renderer.format not in self.accepted_formats:
+            return super(ProjectView, self).list(request, *args, **kwargs)
+        query = Q(parent__isnull=True)
+        if request.GET:
+            if 'status' in request.GET and request.GET['status']:
+                query = query & _get_filter_query(request.GET)
+            else:
+                query = query & Q(status__hidden=False) & _get_filter_query(request.GET)
+        else:
+            query = query & Q(status__hidden=False)
+
+        milestones = Object.filter_by_request(request, Milestone.objects.filter(status__hidden=False))
+        tasks = Object.filter_by_request(request, Task.objects.filter(query))
+        filters = FilterForm(request.user.profile, '', request.GET)
+        context = _get_default_context(request)
+        context.update({'milestones': milestones,
+                        'tasks': tasks,
+                        'filters': filters})
+        context = preprocess_context(context)
+        return Response(context, template_name='projects/index.html')
 
 
 class TaskStatusView(viewsets.ModelViewSet):
