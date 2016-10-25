0c23ac446145567b5379e1763da8bc111691b4d5
avraham <tovmeod@gmail.com>
2016-10-25 14:44:48 +0300

    naming the view with string is deprecated

--- anaf/core/trash/urls.py
+++ anaf/core/trash/urls.py
@@ -2,17 +2,21 @@
 Core module Administration panel URLs
 """
 
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+
+from anaf.core.trash import views
 
 urlpatterns = patterns('anaf.core.trash.views',
-                       url(r'^(\.(?P<response_format>\w+))?/?$',
-                           'index', name='core_trash'),
-                       url(r'^index(\.(?P<response_format>\w+))?/?$',
-                           'index', name='core_trash_index'),
+                       url(r'^/?(\.(?P<response_format>\w+))?/?$', 'index', name='core_trash'),
+                       url(r'^/', include(patterns('',
+                           url(r'^index(\.(?P<response_format>\w+))?/?$', views.index, name='core_trash_index'),
 
-                       # Actions
-                       url(r'^delete/(?P<object_id>\d+)(\.(?P<response_format>\w+))?/?$',
-                           'object_delete', name='core_trash_object_delete'),
-                       url(r'^untrash/(?P<object_id>\d+)(\.(?P<response_format>\w+))?/?$',
-                           'object_untrash', name='core_trash_object_untrash'),
+                           # Actions
+                           url(r'^delete/(?P<object_id>\d+)(\.(?P<response_format>\w+))?/?$',
+                               views.object_delete, name='core_trash_object_delete'),
+                           url(r'^untrash/(?P<object_id>\d+)(\.(?P<response_format>\w+))?/?$',
+                               views.object_untrash, name='core_trash_object_untrash'),
+                                                   )
+                                          )
+                           )
                        )
