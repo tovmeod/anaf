c651999d15625ea1dac5f8e9a905a5e5e5219f19
avraham <tovmeod@gmail.com>
2016-12-11 06:02:32 +0200

    removed unsued import

--- anaf/projects/api/views.py
+++ anaf/projects/api/views.py
@@ -9,7 +9,7 @@ from django.template import RequestContext
 from django.utils.translation import ugettext as _
 from rest_framework import mixins
 from rest_framework import viewsets
-from rest_framework.exceptions import PermissionDenied, MethodNotAllowed
+from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import detail_route, list_route
