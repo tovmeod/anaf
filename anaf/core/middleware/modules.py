ca464fd189e8c3d6ac34fb7b3b3e4d873b45fe13
avraham <tovmeod@gmail.com>
2016-12-12 07:45:09 +0200

    module middleware is not actually an middleware so it doesn't need to be called to all and every request

--- anaf/core/middleware/modules.py
+++ anaf/core/middleware/modules.py
@@ -1,6 +1,7 @@
 """
 Modules middleware: handles modular behavior of the system
 """
+from django.core.exceptions import MiddlewareNotUsed
 
 from anaf.core.conf import settings
 from anaf.core.models import Module
@@ -9,61 +10,67 @@ from anaf.core.models import Module
 # i.e. after Django's own middleware has been fired
 
 
+def check_modules():
+    hmodules = dict()
+    for module in settings.INSTALLED_APPS:
+        import_name = str(module) + "." + settings.ANAF_MODULE_IDENTIFIER
+        try:
+            hmodule = __import__(import_name, fromlist=[str(module)])
+            hmodules[str(module)] = hmodule.PROPERTIES
+        except ImportError:
+            pass
+        except AttributeError:
+            pass
+
+    dbmodules = Module.objects.all()
+
+    for dbmodule in dbmodules:
+        if dbmodule.name not in hmodules:
+            dbmodule.delete()
+        else:
+            differ = False
+            hmodule = dbmodule.name
+            if dbmodule.title != hmodules[hmodule]['title']:
+                dbmodule.title = hmodules[hmodule]['title']
+                differ = True
+            if dbmodule.url != hmodules[hmodule]['url']:
+                dbmodule.url = hmodules[hmodule]['url']
+                differ = True
+            if dbmodule.details != hmodules[hmodule]['details']:
+                dbmodule.details = hmodules[hmodule]['details']
+                differ = True
+            if dbmodule.system != hmodules[hmodule]['system']:
+                dbmodule.system = hmodules[hmodule]['system']
+                differ = True
+            if differ:
+                dbmodule.save()
+
+    for hmodule in hmodules:
+        dbmodule = None
+        try:
+            dbmodule = Module.objects.get(name=hmodule)
+        except Module.DoesNotExist:
+            pass
+        except Module.MultipleObjectsReturned:
+            # Broken database, delete all matching modules
+            Module.objects.filter(name=hmodule).delete()
+        if not dbmodule:
+            dbmodule = Module(name=hmodule, title=hmodules[hmodule]['title'],
+                              url=hmodules[hmodule]['url'],
+                              details=hmodules[hmodule]['details'], system=hmodules[hmodule]['system'])
+            dbmodule.save()
+            dbmodule.set_default_user()
+
+
 class ModuleDetect(object):
     """Handles automatic modules detection"""
-
-    def process_request(self, request):
-        "Process request"
-        hmodules = dict()
-        for module in settings.INSTALLED_APPS:
-            import_name = str(
-                module) + "." + settings.ANAF_MODULE_IDENTIFIER
-            try:
-                hmodule = __import__(import_name, fromlist=[str(module)])
-                hmodules[str(module)] = hmodule.PROPERTIES
-            except ImportError:
-                pass
-            except AttributeError:
-                pass
-
-        dbmodules = Module.objects.all()
-
-        for dbmodule in dbmodules:
-            if dbmodule.name not in hmodules:
-                dbmodule.delete()
-            else:
-                differ = False
-                hmodule = dbmodule.name
-                if dbmodule.title != hmodules[hmodule]['title']:
-                    dbmodule.title = hmodules[hmodule]['title']
-                    differ = True
-                if dbmodule.url != hmodules[hmodule]['url']:
-                    dbmodule.url = hmodules[hmodule]['url']
-                    differ = True
-                if dbmodule.details != hmodules[hmodule]['details']:
-                    dbmodule.details = hmodules[hmodule]['details']
-                    differ = True
-                if dbmodule.system != hmodules[hmodule]['system']:
-                    dbmodule.system = hmodules[hmodule]['system']
-                    differ = True
-                if differ:
-                    dbmodule.save()
-
-        for hmodule in hmodules:
-            dbmodule = None
-            try:
-                dbmodule = Module.objects.get(name=hmodule)
-            except Module.DoesNotExist:
-                pass
-            except Module.MultipleObjectsReturned:
-                # Broken database, delete all matching modules
-                Module.objects.filter(name=hmodule).delete()
-            if not dbmodule:
-                dbmodule = Module(name=hmodule, title=hmodules[hmodule]['title'],
-                                  url=hmodules[hmodule]['url'],
-                                  details=hmodules[hmodule]['details'], system=hmodules[hmodule]['system'])
-                dbmodule.save()
-                dbmodule.set_default_user()
+    def __init__(self):
+        check_modules()
+        # trick so the code only runs once.
+        # see http://stackoverflow.com/questions/2781383/where-to-put-django-startup-code
+        # Can't use AppConfig.ready() because I need to use the DB,
+        # there's an old django ticket on this, conclusion was to reccomend on docs to not use DB in AppConfig.ready()
+        raise MiddlewareNotUsed
 
     def process_response(self, request, response):
         "Process response"
