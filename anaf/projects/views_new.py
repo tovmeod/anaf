96481e56951fd6a4c11d50257cec20fd07fbd60d
avraham <tovmeod@gmail.com>
2016-12-15 14:35:36 +0200

    removed unused import

--- anaf/projects/views_new.py
+++ anaf/projects/views_new.py
@@ -7,7 +7,6 @@ from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.http import Http404
 from django.http import HttpResponseRedirect
-from django.template import RequestContext
 from django.utils.translation import ugettext as _
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
